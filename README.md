c6c8e27610694bfe40ee2317f3c9a89b6cd9d8fc
Alex Racapé <alex.racape@gmail.com>
2024-10-04 22:13:15 -0400

    Cleaned and Added Outline for Login Code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# SlackOverflow-UserLogin
+# User Management Service
+
+This service interfaces with the user database, and it provides login functionality.
+
+
+## Usage
+
+`uvicorn app.main:app --reload`
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,19 +1,38 @@
 from fastapi import APIRouter
 
-from app.models.user import UserLogin  # CourseSection
-from app.resources.user_resource import UserLoginResource  # CourseResource
+from app.models.user import User  # CourseSection
+from app.resources.user_resource import UserResource  # CourseResource
 from app.services.service_factory import ServiceFactory
 
 router = APIRouter()
 
 
 @router.get("/user_info/{user_id}", tags=["users"])
-async def get_users(user_id: str) -> UserLogin:
+async def get_users(id: str) -> User:
 
     # TODO Do lifecycle management for singleton resource
-    res = ServiceFactory.get_service("UserLoginResource")
-    result = res.get_by_key(user_id)
+    res = ServiceFactory.get_service("UserResource")
+    result = res.get_by_key(id)
     return result
+
+
+@router.get("/login/{authorization_code}", tags=["users"])
+async def login(auth_code: str):
+    """Uses Spotify Auth Code to Login User
+
+    Gets login service and sends authorization code to Spotify service
+    The service returns a user model, and this should save it to the database
+    """
+
+    print(f"Trying to login with {auth_code}")
+    spotify = ServiceFactory.get_service("Login")
+    database = ServiceFactory.get_service("UserResourceDataService")
+    user = spotify.get_user_info(auth_code)
+    print(f"Got info: {user}")
+
+    # if user and user not in database: # Eventually add ability to save to database
+    #     database.add(user)
+
 
 """ @router.get("/courses_sections/{course_id}", tags=["users"])
 async def get_courses(course_id: str) -> CourseSection:
@@ -22,5 +41,3 @@ async def get_courses(course_id: str) -> CourseSection:
     res = ServiceFactory.get_service("CourseResource")
     result = res.get_by_key(course_id)
     return result"""
-
-
